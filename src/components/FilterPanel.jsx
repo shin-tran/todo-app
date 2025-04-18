ae7641fb666d486ca23defece8458a6a228847b9
ngocshintrannn <ngoc.shintrann@gmail.com>
2025-04-18 21:24:05 +0700

    fix: wrap filter icons in a div and add colors for better visibility

--- src/components/FilterPanel.jsx
+++ src/components/FilterPanel.jsx
@@ -2,41 +2,46 @@ import React from "react";
 import "../styles/FilterPanel.css";
 import { CgInbox } from "react-icons/cg";
 import { FaCheckSquare, FaFlag, FaTrash } from "react-icons/fa";
+import { IconContext } from "react-icons";
 
 const FilterPanel = () => {
   return (
     <div className="filter-panel">
       <input type="text" name="search-text" id="search-text" />
-      <>
-        <div className="filter-item">
-          <div className="filter-name">
-            <CgInbox />
-            <p>All</p>
+      <div className="filter-container">
+        <IconContext.Provider value={{ className: "filter-panel-icon" }}>
+          <div className="filter-item">
+            <div className="filter-name">
+              <div>
+                <CgInbox color="#fff" />
+              </div>
+              <p>All</p>
+            </div>
+            <p>22</p>
           </div>
-          <p>22</p>
-        </div>
-        <div className="filter-item">
-          <div className="filter-name">
-            <FaFlag />
-            <p>Important</p>
+          <div className="filter-item">
+            <div className="filter-name">
+              <FaFlag color="#ffff00" />
+              <p>Important</p>
+            </div>
+            <p>11</p>
           </div>
-          <p>11</p>
-        </div>
-        <div className="filter-item">
-          <div className="filter-name">
-            <FaCheckSquare />
-            <p>Completed</p>
+          <div className="filter-item">
+            <div className="filter-name">
+              <FaCheckSquare color="#0add0a" />
+              <p>Completed</p>
+            </div>
+            <p>3</p>
           </div>
-          <p>3</p>
-        </div>
-        <div className="filter-item">
-          <div className="filter-name">
-            <FaTrash />
-            <p>Delete</p>
+          <div className="filter-item">
+            <div className="filter-name">
+              <FaTrash color="#fb3535" />
+              <p>Delete</p>
+            </div>
+            <p>1</p>
           </div>
-          <p>1</p>
-        </div>
-      </>
+        </IconContext.Provider>
+      </div>
     </div>
   );
 };
