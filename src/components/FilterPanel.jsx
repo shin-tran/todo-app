6c55bf3e255539cf998dc109a23b8f7364d5e33e
ngocshintrannn <ngoc.shintrann@gmail.com>
2025-04-18 21:03:35 +0700

    refactor: update import paths for styles and add new CSS files for FilterPanel and Sidebar components

--- src/components/FilterPanel.jsx
+++ src/components/FilterPanel.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import "../styles/FilterPanel.css"
 
 const FilterPanel = () => {
   return (
     <div className="filter-panel">
       <input type="text" name="search-text" id="search-text" />
+      <>
+        <div className="filter-item">
+          <div className="filter-name">
+            <img src="" alt="" />
+            <p>All</p>
+          </div>
+          <p>22</p>
+        </div>
+      </>
     </div>
   );
 };
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./Sidebar.css";
+import "../styles/Sidebar.css";
 
 const Sidebar = (props) => {
   const data = props.todoItem;
