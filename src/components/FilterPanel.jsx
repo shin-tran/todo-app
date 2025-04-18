26a53b0fc408058be7af5c8b4ede9400b712a889
ngocshintrannn <ngoc.shintrann@gmail.com>
2025-04-18 21:51:07 +0700

    feat: implement dynamic filter items in FilterPanel component

--- src/components/FilterPanel.jsx
+++ src/components/FilterPanel.jsx
@@ -1,45 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/FilterPanel.css";
 import { CgInbox } from "react-icons/cg";
 import { FaCheckSquare, FaFlag, FaTrash } from "react-icons/fa";
 import { IconContext } from "react-icons";
 
 const FilterPanel = () => {
+  const [selectedFilterId, setSelectedFilterId] = useState("all");
+
+  const filterItems = [
+    { id: "all", label: "All", icon: <CgInbox color="#fff" /> },
+    { id: "important", label: "Important", icon: <FaFlag color="#ffff00" /> },
+    {
+      id: "completed",
+      label: "Completed",
+      icon: <FaCheckSquare color="#0add0a" />,
+    },
+    { id: "delete", label: "Delete", icon: <FaTrash color="#fb3535" /> },
+  ];
+
   return (
     <div className="filter-panel">
       <input type="text" name="search-text" id="search-text" />
       <div className="filter-container">
         <IconContext.Provider value={{ className: "filter-panel-icon" }}>
-          <div className="filter-item">
-            <div className="filter-name">
-              <div>
-                <CgInbox color="#fff" />
+          {filterItems.map((item, index) => {
+            return (
+              <div
+                key={`${item.id}-${index}`}
+                className={`filter-item ${
+                  item.id === selectedFilterId ? "selected" : ""
+                }`}
+              >
+                <div className="filter-name">
+                  <div>{item.icon}</div>
+                  <p>{item.label}</p>
+                </div>
+                <p>22</p>
               </div>
-              <p>All</p>
-            </div>
-            <p>22</p>
-          </div>
-          <div className="filter-item">
-            <div className="filter-name">
-              <FaFlag color="#ffff00" />
-              <p>Important</p>
-            </div>
-            <p>11</p>
-          </div>
-          <div className="filter-item">
-            <div className="filter-name">
-              <FaCheckSquare color="#0add0a" />
-              <p>Completed</p>
-            </div>
-            <p>3</p>
-          </div>
-          <div className="filter-item">
-            <div className="filter-name">
-              <FaTrash color="#fb3535" />
-              <p>Delete</p>
-            </div>
-            <p>1</p>
-          </div>
+            );
+          })}
         </IconContext.Provider>
       </div>
     </div>
